01aa6c7b354d0078a7895c1b6684675fbabefb4d
mostafaamahmoudd <mostafaa.mahmoudd550@gmail.com>
2026-04-12 17:04:40 +0200

    wip

---
+++ app/Models/Api/V1/InstrumentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Api\V1;
+
+use App\Traits\ApiResponse;
+use Illuminate\Database\Eloquent\Model;
+
+class InstrumentController extends Model
+{
+    use ApiResponse;
+
+    public function index()
+    {
+        
+    }
+}
